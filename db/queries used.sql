7ea8c9c09812e9619843340f06b4407fc3750295
Benito <benitocavazosr@yahoo.com>
2022-02-06 21:52:42 -0600

    adding query

--- db/queries used.sql
+++ db/queries used.sql
@@ -1,4 +1,4 @@
-SELECT employee.id, employee.first_name, employee.last_name, role.title, departament.name departament, role.salary, employee.manager_id as manager
+SELECT employee.id, employee.first_name, employee.last_name, role.title, departament.name departament, role.salary, e2.* as manager
 FROM employee
 LEFT JOIN role ON employee.role_id = role.id
 LEFT JOIN departament ON role.departament_id = departament.id
@@ -7,9 +7,18 @@ LEFT JOIN (SELECT id, concat(employee.first_name,' ',employee.last_name) aname F
 concat(employee.first_name,' ',employee.last_name) manager
 
 
-select * from employee
-left join (SELECT id, concat(employee.first_name,' ',employee.last_name) aname FROM employee) e2 on employee.manager_id = e2.id
+    select * from employee
+    left join (SELECT id, concat(employee.first_name,' ',employee.last_name) as name FROM employee) e2 on employee.manager_id = e2.id
+    ;
+
+SELECT id, concat(employee.first_name,' ',employee.last_name) as name FROM employee e2 on employee.manager_id = e2.id
+
+SELECT employee.manager_id = concat
+
+select employee.* from employee
+left join (SELECT id, concat(employee.first_name,' ',employee.last_name) as name FROM employee) e2 on employee.manager_id = e2.id
 ;
+
 
 
 select employee.id, employee.first_name, employee.last_name, role.title, departament.name departament, employee.manager_id as manager from employee
@@ -35,3 +44,18 @@ WHERE concat(employee.first_name,' ', employee.last_name) = 'Tom Allen';
 SELECT role.id
 from role
 WHERE 
+
+
+
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title, departament.name departament, role.salary, concat(m.first_name,' ',m.last_name) manager
+FROM employee
+LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN departament ON role.departament_id = departament.id
+left join employee as m on m.id  = employee.manager_id
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title, departament.name departament, role.salary, concat(m.first_name,' ',m.last_name) manager
+    -> FROM employee
+    -> LEFT JOIN role ON employee.role_id = role.id
+    -> LEFT JOIN departament ON role.departament_id = departament.id
+    -> inner join employee as m on employee.manager_id = m.id;
